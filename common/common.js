6d03f88b8e8511dc1c788157ea8409f66e5bf623
lizhiming1991 <13372077095@163.com>
2019-06-18 09:39:06 +0800

    lzm合并提交2

--- common/common.js
+++ common/common.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-=======
+
 
 // export const ImgUrl = "http://sitadmin.dailyld.com";
->>>>>>> a004988a8e16dc4ea9fda8a59174349d8a6c715c
 export const ImgUrl = "http://192.168.0.210";
 export const onlineURL = "http://192.168.0.210:9999";
 
