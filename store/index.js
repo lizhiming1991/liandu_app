6e3483ec60d755049df729edf3a99b170a7c4183
wangcong <986888783@qq.com>
2019-06-18 09:40:12 +0800

    cw 企业 bug修改

--- store/index.js
+++ store/index.js
@@ -9,11 +9,7 @@ const store = new Vuex.Store({
 		hasLogin: false,
 		loginProvider: "",
 		openid: null,
-<<<<<<< HEAD
-		userid: "1329",
-=======
-		userid: 1348,
->>>>>>> a004988a8e16dc4ea9fda8a59174349d8a6c715c
+		userid: "",
 		phone:"",
 		modifyRandomNumbers:"",
 	},
