70ff495af7b575e9ccc9a78eb5b5985127310a43
wangcong <986888783@qq.com>
2019-06-27 19:51:54 +0800

    王聪 0627 修4

--- store/index.js
+++ store/index.js
@@ -9,7 +9,7 @@ const store = new Vuex.Store({
 		hasLogin: false,
 		loginProvider: "",
 		openid: null,
-		userid: "1329",
+		userid: "",
 		phone: "",
 		phoneNumber: "",
 		modifyRandomNumbers:"", 
